ef39d114b40e1cc85d06a4a1ccc71c9c37af7b20
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-09-16 15:26:14 +0530

    fix : logo issue

--- src/components/about/FunFact.js
+++ src/components/about/FunFact.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BsTrophyFill } from "react-icons/bs";
 import { SiAntdesign } from "react-icons/si";
 import { BiCodeAlt } from "react-icons/bi";
-import { IoLogoYoutube } from "react-icons/io";
+import { SiLeetcode } from "react-icons/si";
 import FunFactCard from "./FunFactCard";
 
 const FunFact = () => {
   return (
     <div className="px-0 grid grid-cols-2 md:grid-cols-4 pb-10">
-      <FunFactCard icon={<BsTrophyFill />} des="Certification of Heakerrank" />
+      <FunFactCard icon={<BsTrophyFill />} des="Certifications of Heakerrank" />
       <FunFactCard icon={<SiAntdesign />} des="3 Finished Projects" />
       <FunFactCard icon={<BiCodeAlt />} des="100 hours of coding" />
-      <FunFactCard icon={<IoLogoYoutube />} des="300+ question solve on leetcode" />
+      <FunFactCard icon={<SiLeetcode />} des="300+ question solve on leetcode" />
     </div>
   );
 };
